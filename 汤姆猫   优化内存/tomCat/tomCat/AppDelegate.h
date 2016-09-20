add771913c372dda547d4f3b78b969cac9a7ba67
史夏杰 <pilitws@gmail.com>
2016-09-21 01:09:12 +0800

    完成 tomcat

---
+++ 汤姆猫   优化内存/tomCat/tomCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tomCat
+//
+//  Created by 史夏杰 on 16/8/27.
+//  Copyright © 2016年 study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 汤姆猫   优化内存/tomCat/tomCat/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  tomCat
+//
+//  Created by 史夏杰 on 16/8/27.
+//  Copyright © 2016年 study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *tom;
+- (IBAction)btnClick:(UIButton *)sender;
+
+@end
+
